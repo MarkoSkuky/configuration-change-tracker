65a0ddbc7c25ab97cc4c06dff0e97a5becd6fbb5
Marko Skukálek <markoskukalek1@gmail.com>
2026-05-24 21:42:52 +0200

    Initial springboot project setup

---
+++ src/main/java/com/marko/configurationchangetracker/ConfigurationChangeTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.marko.configurationchangetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigurationChangeTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigurationChangeTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/marko/configurationchangetracker/ConfigurationChangeTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marko.configurationchangetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationChangeTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
